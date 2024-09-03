1b81b029a280d09b6761a8f36a0f9e82184b3c9a
Tạ Tuấn Anh <ovftank@gmail.com>
2024-09-04 00:58:50 +0700

    feat: change config nginx

--- setup.sh
+++ setup.sh
@@ -21,7 +21,10 @@ server {
     server_name _;
 
     location / {
-        proxy_pass http://localhost:5000;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_pass http://localhost:8000;
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection 'upgrade';
